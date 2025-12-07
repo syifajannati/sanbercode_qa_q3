2becaedf6bd70ec4f70898cd9d5773d22cf0f195
syifajannati <syifajannati@gmail.com>
2025-12-07 22:37:43 +0700

    add TC 004

--- cypress/e2e/orangehrm_login.cy.js
+++ cypress/e2e/orangehrm_login.cy.js
@@ -30,15 +30,11 @@ describe('Scenario Login OrangeHRM', () => {
 
     // get kolom input username
     // & input `Admin`
-    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    //   .type('Admin')
     cy.get("input[placeholder='Username']")
       .type('Admin')
 
     // get kolom input password
     // then input `admin123`
-    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    //   .type('admin123')
     cy.get("input[placeholder='Password']")
       .type('admin123')
 
@@ -56,19 +52,42 @@ describe('Scenario Login OrangeHRM', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
     // get kolom input username
-    // & input `Admin`
-    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    //   .type('admin123')
+    // & input `admin123`
     cy.get("input[placeholder='Username']")
       .type('admin123')
 
     // get kolom input password
-    // then input `admin123`
+    // then input `Admin`
     // then click Enter
-    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
-    //   .type('Admin{enter}')
     cy.get("input[placeholder='Password']")
       .type('Admin{enter}')
+
+    // tunggu hingga alert muncul, lalu periksa kontennya
+    // memastikan ada `Invalid credentials` pada alert
+    // (tidak berhasil login)
+    cy.get('.oxd-alert-content', { timeout: 10000 }) // timeout opsional, menunggu hingga 10 detik
+      .should('be.visible')
+      .and('contain', 'Invalid credentials')
+  });
+
+  // TEST CASE 004
+  it('TC 004 - Login menggunakan username dan password yang tidak valid dengan klik Login', () => {
+    // buka halaman orangehrm login
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    // get kolom input username
+    // & input `admin123`
+    cy.get("input[placeholder='Username']")
+      .type('admin123')
+
+    // get kolom input password
+    // then input `Admin`
+    cy.get("input[placeholder='Password']")
+      .type('Admin')
+
+    // get login button 
+    // & click Login
+    cy.get('.oxd-button').click()
 
     // tunggu hingga alert muncul, lalu periksa kontennya
     // memastikan ada `Invalid credentials` pada alert
