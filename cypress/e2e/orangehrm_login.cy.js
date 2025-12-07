76851f1cec203db30a911c79f4cc44dfd4366c3e
syifajannati <syifajannati@gmail.com>
2025-12-07 22:31:26 +0700

    add TC 003

--- cypress/e2e/orangehrm_login.cy.js
+++ cypress/e2e/orangehrm_login.cy.js
@@ -6,13 +6,17 @@ describe('Scenario Login OrangeHRM', () => {
 
     // get kolom input username
     // & input `Admin`
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('Admin')
+    cy.get("input[placeholder='Username']")
       .type('Admin')
 
     // get kolom input password
     // then input `admin123`
     // then click Enter
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('admin123{enter}')
+    cy.get("input[placeholder='Password']")
       .type('admin123{enter}')
 
     // memastikan user berada di halaman dashboard (berhasil login)
@@ -26,12 +30,16 @@ describe('Scenario Login OrangeHRM', () => {
 
     // get kolom input username
     // & input `Admin`
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('Admin')
+    cy.get("input[placeholder='Username']")
       .type('Admin')
 
     // get kolom input password
     // then input `admin123`
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('admin123')
+    cy.get("input[placeholder='Password']")
       .type('admin123')
 
     // get login button 
@@ -40,5 +48,33 @@ describe('Scenario Login OrangeHRM', () => {
 
     // memastikan user berada di halaman dashboard (berhasil login)
     cy.url().should('include', 'dashboard')
+  });
+
+  // TEST CASE 003
+  it('TC 003 - Login menggunakan username dan password yang tidak valid dengan klik Enter', () => {
+    // buka halaman orangehrm login
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    // get kolom input username
+    // & input `Admin`
+    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('admin123')
+    cy.get("input[placeholder='Username']")
+      .type('admin123')
+
+    // get kolom input password
+    // then input `admin123`
+    // then click Enter
+    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+    //   .type('Admin{enter}')
+    cy.get("input[placeholder='Password']")
+      .type('Admin{enter}')
+
+    // tunggu hingga alert muncul, lalu periksa kontennya
+    // memastikan ada `Invalid credentials` pada alert
+    // (tidak berhasil login)
+    cy.get('.oxd-alert-content', { timeout: 10000 }) // timeout opsional, menunggu hingga 10 detik
+      .should('be.visible')
+      .and('contain', 'Invalid credentials')
   });
 })
