bb2b0a21b72c6bc378b527404e238c3469716364
syifajannati <syifajannati@gmail.com>
2025-12-07 21:59:02 +0700

    TC 001

--- cypress/e2e/orangehrm_login.cy.js
+++ cypress/e2e/orangehrm_login.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Scenario Login OrangeHRM', () => {
+  it('TC 001 - Login dengan username dan password yang valid', () => {
+    // buka halaman orangehrm login
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    // get kolom input username
+    // & input `Admin`
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+      .type('Admin')
+
+    // get kolom input password
+    // & input `admin123`
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input')
+      .type('admin123')
+
+    // get login button 
+    // & click
+    cy.get('.oxd-button').click()
+
+    // memastikan user berada di halaman dashboard (berhasil login)
+    cy.url().should('include', 'dashboard')
+  });
 })
